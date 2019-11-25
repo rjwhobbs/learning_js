df8a5801ee7f225f91fbc2438eacb31a9b760448
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-25 07:53:37 +0200

    Do you even export

--- node_basics/helpers.js
+++ node_basics/helpers.js
@@ -1,4 +1,27 @@
-function is_prime(n) {
+// function is_prime(n) {
+// 	if (n == 2)
+// 		return 1;
+// 	if (n == 3)
+// 		return 1
+// 	if (n % 2 == 0)
+// 		return 0;
+// 	if (n % 3 == 0)
+// 		return 0;
+	
+// 	let i = 5;
+// 	let w = 2;
+
+// 	while (i * i <= n)
+// 	{
+// 		if (n % i == 0)
+// 			return 0;
+// 		i += w;
+// 		w = 6 - w;
+// 	}
+// 	return 1;
+// }
+
+exports.is_prime = (n) => {
 	if (n == 2)
 		return 1;
 	if (n == 3)
@@ -21,6 +44,9 @@ function is_prime(n) {
 	return 1;
 }
 
-module.exports = {
-	is_prime
-}
+exports.square = (n) => n * n;
+
+// module.exports = {
+// 	is_prime,
+// 	square
+// }
