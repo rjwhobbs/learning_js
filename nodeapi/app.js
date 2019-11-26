dfba7817677886cb9ab886d050b91004fba38669
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-26 08:55:04 +0200

    Do you even .env

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const postRoutes = require ('./routes/posts');
 const morgan = require ('morgan'); 
+const dotenv = require ('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true})
+.then(() => console.log('DB connected'));
+
+mongoose.connection.on('error', err => {
+	console.log(`Error connecting to the DB: ${err.message}`);
+});
 
 app.use(morgan('dev'));
 app.use('/', postRoutes);
 
 //app.get('/', getPosts);
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log("Stuff");
 });
