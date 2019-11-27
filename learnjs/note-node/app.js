2e021b513e68c71f4f8ce47055e4d43f6bb91778
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-11-27 17:04:10 +0200

    Gotta delete these mods

--- learnjs/note-node/app.js
+++ learnjs/note-node/app.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const func = require('./notes.js');
 
-var user = os.userInfo();
+console.log(_.isString(true));
+console.log(_.isString('true'));
+console.log(_.isString('1'));
+console.log(_.isString(1));
+//console.log(func.add(11, -5));
+// var user = os.userInfo();
 
-console.log(user);
+// console.log(user);
 
-fs.appendFile('message.txt', 'howzit ' + user.username, (err) => {
-	if (err) throw err;
-});
+// fs.appendFile('message.txt', 'howzit ' + user.username, (err) => {
+// 	if (err) throw err;
+// });
---
+++ learnjs/test.js
@@ -0,0 +1,24 @@
+// if (1) {
+// 	var x = 5;
+// }
+
+// console.log(x);
+
+// var myvar = 'my value';
+ 
+// function test() { // variable hoisting , not sure I totally get it
+//   var myvar;
+//   console.log(myvar); // undefined
+//   myvar = 'local value';
+// };
+
+//test();
+
+// foo(); // "bar"
+
+// function foo() { // function hoisting
+//   console.log('bar');
+// }
+
+// var marray = ['a', 'b', 'c'];
+// marray.push('e', 'f');
