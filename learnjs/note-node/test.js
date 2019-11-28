b9643dad1d8b99e4f9a64274fff120485ceae21e
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-28 16:13:48 +0200

    This js stuff

--- learnjs/note-node/test.js
+++ learnjs/note-node/test.js
@@ -1,11 +1,11 @@
 var arr = [];
 var cat = 'cat';
 
-var test = '{"this" : "that"}';
+var test = '[{"this" : "that"}]';
 var test2 = {
 	cat	
 };
 
 arr = JSON.parse(test);
-
-console.log(typeof arr);
+arr.push(test2);
+console.log(arr);
