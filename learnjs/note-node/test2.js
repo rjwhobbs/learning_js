86b95ebbd0a4958388a30fd4ad9134c46987120a
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-05 10:25:28 +0200

    Stuffs

--- learnjs/note-node/test2.js
+++ learnjs/note-node/test2.js
@@ -1,13 +1,38 @@
-function test(message, callback) {
-	console.log(`This was your argument: ${message}`);
+// function test(message, callback) {
+// 	console.log(`This was your argument: ${message}`);
 
-	if (typeof callback === "function") {
-		callback(217, "I am an argument");
+// 	if (typeof callback === "function") {
+// 		callback(message);
+// 	}
+// };
+
+// test("howzit", function(str) {
+// 	console.log("I am the callback");
+// 	console.log(str);
+// });
+
+let dataArr = [];
+
+let uData = {
+	name : "Joe",
+	ocupation : "Farmer",
+	location : "Brakpan"
+}
+
+function logStuff (stuff) {
+	for (let i in stuff) {
+		console.log(`${i} ${stuff[i]}`);
 	}
-};
+}
 
-test("howzit", function(num, str) {
-	console.log("I am the callback");
-	console.log(num);
-	console.log(str);
-});
+function doStuff (things, callback) {
+	dataArr.push(things);
+	if (typeof callback === "function") {
+		callback(things);
+	}
+}
+
+doStuff(uData, logStuff);
+console.log(dataArr);
+
+
